30f623bbc13004686559811892a574e639501710
Lauren Allpress <lallpress@unomaha.edu>
2017-07-05 23:25:00 +0000

    dropped 'donations' model and table'

--- app/models/donation.rb
+++
@@ -1,4 +0,0 @@
-class Donation < ActiveRecord::Base
-      validates :item, presence: true, length: {minimum: 4 }
-    validates :quantity, presence: true 
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705203048) do
-
-  create_table "donations", force: :cascade do |t|
-    t.string   "item"
-    t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170705232254) do
 
   create_table "items", force: :cascade do |t|
     t.string   "category"
