54f142c9e226ded04af5d3fdb1fa4b17df7a8600
Lauren Allpress <lallpress@unomaha.edu>
2017-07-06 23:00:49 +0000

    Refactor files related to organizations to use plural and singular in the same way as items.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,3 @@
 class Organization < ActiveRecord::Base
     validates :org_name, presence: true
-    validates :contact_id, presence: true
 end
--- test/controllers/organizations_controller_test.rb
+++ test/controllers/organizations_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OrganizationControllerTest < ActionController::TestCase
+class OrganizationsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
