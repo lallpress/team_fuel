7f0da0bc2135900bce6369a37ca0068a73f6e093
i_am_groot <software.4.systems@gmail.com>
2017-07-28 09:03:55 +0000

    Inventory CRUD, new Gems, and a seed that will create 100 Inventory records.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,6 @@
 class Inventory < ActiveRecord::Base
+    validates_presence_of :itemnum, :orgnum, :quantity, :expires
+    has_many :items
+    belongs_to :items
+    delegate :itemName, :category, to: :items
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Item.connect
+#Item.destroy_all
+#Inventory.connect
+#Inventory.destroy_all
+ 
+100.times do |index|
+    Inventory.create!(itemnum: index,
+                        orgnum: Faker::Number.non_zero_digit,
+                        quantity: Faker::Number.between(from = 1.00, to = 5000.00),
+                        expires: Faker::Time.between(1.week.ago, 4.months.from_now ))
+#  Item.create!(category: Faker::Name.name,
+#                itemName: Faker::Name.name)
+end
+p "Created #{Inventory.count} items"
+p "Created #{Item.count} items"
